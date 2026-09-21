53b5dfa8919d38458680bba3abb146b118c9f66a
andres <anreszamorano8@gmail.com>
2026-09-20 21:33:19 -0700

    Practica 02 Terminada

--- Practica02/app/src/main/java/com/example/calculadora/MainActivity.kt
+++ Practica02/app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -18,30 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CalculadoraTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            CalculatorScreen()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CalculadoraTheme {
-        Greeting("Android")
     }
 }
